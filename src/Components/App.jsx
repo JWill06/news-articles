3b32828d3d4dbd9e3f5ea48b392c025cc444d173
jordan williamson <williamsonjordan05@gmail.com>
2024-08-10 11:58:57 -0600

    showing articles on page with filtering

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,11 +1,13 @@
 import '../Styles/App.css';
 import { Route, Routes } from 'react-router-dom';
 import Articles from './Articles';
+import Navbar from './Navbar';
 
 function App() {
   
   return (
     <div className="App">
+      <Navbar />
       <header className="App-header">
         <Routes>
         <Route path='/' element={<Articles />}/>
